ba381a596a832a2b6b71ab4f981bd8bd4264bccc
Rumman Parvez <116460823+rummanparvez@users.noreply.github.com>
2025-09-05 17:10:54 +0530

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://infinite-locus-9r1h.onrender.com/api';
+const API_BASE_URL = 'https://infinite-locus-1.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
